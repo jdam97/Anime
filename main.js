7c4b6bc886d7e00c1a80651b402b483cedba6a3b
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-05-21 12:11:41 -0500

    feat: :sparkles: search by genre
    add section search by genre

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const btn = document.querySelector('#btn')
 let genero = document.querySelector('#genero');
 //cards
 let contenedorCards = document.querySelector('#contenedor-cards')
+let tituloGenre = document.querySelector('#hero-title h2')
 
 const options = {
     method: 'GET',
@@ -14,34 +15,56 @@ const options = {
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
-  
+    tituloGenre.innerHTML = (genero.value).toUpperCase()
+    searchAnimeByGenre(genero.value)
 
 })
+
+const searchAnimeByGenre = async (genre) => {
+    options.method = "GET";
+    let animes = await (await fetch(`https://anime-db.p.rapidapi.com/anime?page=1&size=20&genres=${genre}`, options)).json();
+    contenedorCards.innerHTML = ''
+    for (let i = 0; i < animes.data.length; i++){
+        contenedorCards.innerHTML += `
+        <div class="card">
+            <img src="${animes.data[i].image}" alt="${animes.data[i].title}">
+            <h2>${animes.data[i].title}</h2>
+        </div>
+        `
+    }
+}
 
 //Get generos
 const getGeneros = async () => {
     options.method = "GET";
-    let generos = await (await fetch(`https://anime-db.p.rapidapi.com/genre`,options)).json();
+    let generos = await (await fetch(`https://anime-db.p.rapidapi.com/genre`, options)).json();
     console.log(generos._id)
     for (let i = 0; i < generos.length; i++) {
-        genero.innerHTML += `<option value=${generos[i]._id}">${generos[i]._id}</option>`
+        genero.innerHTML += `<option value=${generos[i]._id}>${generos[i]._id}</option>`
     }
+
 }
 
 //Get all
 const getAllAnimes = async () => {
     options.method = "GET";
-    let animes = await (await fetch(`https://anime-db.p.rapidapi.com/anime?page=1&size=20&sortOrder=asc`,options)).json();
-    for(let i=0;i<animes.data.length;i++){
+    let animes = await (await fetch(`https://anime-db.p.rapidapi.com/anime?page=1&size=20&sortOrder=asc`, options)).json();
+    for (let i = 0; i < animes.data.length; i++) {
         contenedorCards.innerHTML += `
         <div class="card">
             <img src="${animes.data[i].image}" alt="haikyu">
-            <h2>${animes.data[i].title}!!!</h2>
+            <h2>${animes.data[i].title}</h2>
         </div>
         `
     }
- }
- //invocaciones funciones
+}
+
+function searchGenre() {
+
+}
+
+//invocaciones funciones
+
 // getGeneros();
-//  getAllAnimes()
+// getAllAnimes()
 
